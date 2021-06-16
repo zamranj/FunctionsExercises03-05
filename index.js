46b36983f9a5be1ee622ce22986a37b89fc15d55
zeejay21 <84418586+zamranj@users.noreply.github.com>
2021-06-16 01:53:38 +0000

    complete

--- index.js
+++ index.js
@@ -8,11 +8,33 @@
 //f) Use method chaining to reduce the lines of code within the function.
 
 
+function reverseCharacters(input){
+
+  if(typeof(input)==="string"){
+    return input.split("").reverse().join("");
+  }
+  if(typeof(input)==="number"){
+    return Number(input.toString().split("").reverse().join(""))
+  }
+}
+
+  // return letterArray = str.split("").reverse().join("");
+
+
+console.log(reverseCharacters(13));
+console.log(reverseCharacters("apple"));
+console.log(reverseCharacters("LC101"));
+console.log(reverseCharacters("Capitalized Letters"));
+console.log(reverseCharacters("I love the smell of code in the morning"));
+
 
 //2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
 //a) Add an if statement to your reverseCharacters function to check the typeof the parameter.
 //b - d) If typeof is ‘string’, return the reversed string as before. If typeof is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
 //e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+
+
+
 
 //3) Create a new function with one parameter, which is the array we want to change. The function should:
 //a) Define and initialize an empty array.
@@ -21,9 +43,17 @@
 //d) Add the reversed string (or number) to the array defined in part ‘a’.
 //e) Return the final, reversed array.
 //f) Be sure to print the results from each test case in order to verify your code.
+function reverseArray(array){
+  let out = [];
+  for(let i = 0; i<array.length; i++){
+    out.unshift(reverseCharacters(array[i]))
+  }
+  return out;
+}
 
-
-
-let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
-let arrayTest2 = [123, 8897, 42, 1168, 8675309];
-let arrayTest3 = ['hello', 'world', 123, 'orange'];
+let array1 = ['apple', 'potato', 'Capitalized Words'];
+let array2 = [123, 8897, 42, 1168, 8675309];
+let array3 = ['hello', 'world', 123, 'orange'];
+console.log(reverseArray(array1))
+console.log(reverseArray(array2))
+console.log(reverseArray(array3))
